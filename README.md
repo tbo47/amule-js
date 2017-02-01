4df5cdc26641add40be57ee7e99ca4236dc1c9b3
Thibaut Lassalle <thibaut.lassalle@gmail.com>
2017-02-01 14:13:50 +0700

    use of ES6 language

--- README.md
+++ README.md
@@ -3,13 +3,13 @@
 Javascript API to connect to amule. It exposes a couple of [Promises](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise) to interact with amule. It uses [chrome.sockets.tcp](https://developer.chrome.com/apps/sockets_tcp).
 
 ```javascript
-aMule.connect('127.0.0.1', 4712, 'password', md5).then(function(m){
+aMule.connect('127.0.0.1', 4712, 'password', md5).then(m => {
   // you are connected
 });
 ```
 
 ```javascript
-aMule.getSharedFiles().then(function(listOfFileJSON){
+aMule.getSharedFiles().then(listOfFileJSON => {
   // parse json file
 });
 ```
@@ -19,13 +19,13 @@ aMule.search('search key words');
 ```
 
 ```javascript
-aMule.fetchSearch().then(function(listOfFileJSON){
+aMule.fetchSearch().then(listOfFileJSON => {
   // parse json file
 })
 ```
 
 ```javascript
-aMule.getDownloads().then(function(listOfFileJSON){
+aMule.getDownloads().then(listOfFileJSON => {
   // parse json file
 });
 ```
--- amule-js.js
+++ amule-js.js
@@ -6,12 +6,12 @@
 
   var $ = exports.aMule = {};
   $.data = {
-      isConnected: false
+    isConnected: false
   };
   var offset = 0;
   var socketId;
 
-  var init = function(password, md5) {
+  const _init = (password, md5) => {
     $.md5 = md5;
     // must be the same as ECPassword in .aMule/amule.conf
     $.data.md5 = $.md5(password);
@@ -25,12 +25,12 @@
   /**
    * from amule ECCodes.h code
    */
-  var ECCodes = {
+  const ECCodes = {
     EC_CURRENT_PROTOCOL_VERSION : 0x0204,
     EC_OP_AUTH_REQ : 0x02,
     EC_OP_GET_SHARED_FILES : 0x10
   };
-  var ECOpCodes = {
+  const ECOpCodes = {
     EC_OP_STRINGS : 0x06,
     EC_TAGTYPE_UINT16 : 0x03,
     EC_TAGTYPE_UINT8 : 2, // defined in ECTagTypes.h
@@ -43,22 +43,22 @@
     EC_OP_SEARCH_PROGRESS : 0x29,
     EC_OP_DOWNLOAD_SEARCH_RESULT : 0x2A
   };
-  var ECTagNames = {
+  const ECTagNames = {
     EC_TAG_CLIENT_NAME : 0x0100,
     EC_TAG_CLIENT_VERSION : 0x0101,
     EC_TAG_PROTOCOL_VERSION : 0x0002,
     EC_TAG_PASSWD_HASH : 0x0001
   };
-  var ProtocolVersion = {
+  const ProtocolVersion = {
     EC_CURRENT_PROTOCOL_VERSION : 0x0204
   };
-  var EC_SEARCH_TYPE = {
+  const EC_SEARCH_TYPE = {
     EC_SEARCH_LOCA : 0x00,
     EC_SEARCH_GLOBAL : 0x01,
     EC_SEARCH_KAD : 0x02,
     EC_SEARCH_WEB : 0x03
   };
-  var EC_TAG_SEARCHFILE = {
+  const EC_TAG_SEARCHFILE = {
     EC_TAG_SEARCH_TYPE : 0x0701,
     EC_TAG_SEARCH_NAME : 0x0702,
     EC_TAG_SEARCH_MIN_SIZE : 0x0703,
@@ -73,7 +73,7 @@
   /**
    * Used internally to build a request
    */
-  var _buildTagArrayBuffer = function(ecTag, ecOp, value, children) {
+  const _buildTagArrayBuffer = (ecTag, ecOp, value, children) => {
     $.data.recurcifInBuildTagArrayBuffer++;
     var tagLength = 0;
     var dv = new DataView(new ArrayBuffer(Uint16Array.BYTES_PER_ELEMENT));
@@ -165,7 +165,7 @@
   /**
    * Build request headers
    */
-  var _setHeadersToRequest = function(opCode) {
+  const _setHeadersToRequest = function(opCode) {
     var dv = new DataView(new ArrayBuffer(Uint32Array.BYTES_PER_ELEMENT));
     // set flags, normal === 32 (34 pour amule-gui)
     dv.setUint32(0, 32, false);
@@ -185,7 +185,7 @@
    *
    * @returns {ArrayBuffer}
    */
-  $._finalizeRequest = function(tagCount) {
+  const _finalizeRequest = tagCount => {
     // calculating the buffer length in bytes
     var bufferLength = 0;
     for (var i = 0; i < $.data.arrayBuffers.length; i++) {
@@ -219,16 +219,16 @@
    *
    * @returns {ArrayBuffer}
    */
-  var getAuthRequest1 = function() {
+  const getAuthRequest1 = function() {
     _setHeadersToRequest(2);//EC_OP_AUTH_REQ
-    var tagCount = 0;
+    let tagCount = 0;
     _buildTagArrayBuffer(ECTagNames.EC_TAG_CLIENT_NAME*2, ECOpCodes.EC_OP_STRINGS, "amule-js\0", null);
     tagCount++;
     _buildTagArrayBuffer(ECTagNames.EC_TAG_CLIENT_VERSION*2, ECOpCodes.EC_OP_STRINGS, "1.0\0", null);
     tagCount++;
     _buildTagArrayBuffer(4, ECOpCodes.EC_TAGTYPE_UINT16, ProtocolVersion.EC_CURRENT_PROTOCOL_VERSION, null);
     tagCount++;
-    return $._finalizeRequest(tagCount);
+    return _finalizeRequest(tagCount);
   };
 
   /**
@@ -236,18 +236,18 @@
    *
    * @returns {ArrayBuffer}
    */
-  $.getAuthRequest2 = function() {
+  const _getAuthRequest2 = () => {
     _setHeadersToRequest(80);
-    var tagCount = 0;
+    let tagCount = 0;
     _buildTagArrayBuffer(2, ECOpCodes.EC_TAGTYPE_HASH16, $.md5($.data.md5 + $.md5($.data.solt)), null);
     tagCount++;
-    return $._finalizeRequest(tagCount);
+    return _finalizeRequest(tagCount);
   };
 
   /**
    *
    */
-  $.getSearchStartRequest = function(q) {
+  const _getSearchStartRequest = q => {
     _setHeadersToRequest(ECOpCodes.EC_OP_SEARCH_START);
     var tagCount = 0;
     var children = [];
@@ -272,64 +272,75 @@
 
     _buildTagArrayBuffer(EC_TAG_SEARCHFILE.EC_TAG_SEARCH_TYPE, ECOpCodes.EC_TAGTYPE_UINT8, EC_SEARCH_TYPE.EC_SEARCH_LOCA, children);
     tagCount++;
-    return $._finalizeRequest(tagCount);
+    return _finalizeRequest(tagCount);
   };
 
   /**
    *
    */
-  var getSharedFilesRequest = function() {
+  const getSharedFilesRequest = () => {
     _setHeadersToRequest(ECCodes.EC_OP_GET_SHARED_FILES);
     var tagCount = 0;
     // _buildTagArrayBuffer(8, ECOpCodes.EC_TAGTYPE_UINT8, EC_SEARCH_TYPE.EC_SEARCH_LOCA, null);
     // tagCount++;
-    return $._finalizeRequest(tagCount);
+    return _finalizeRequest(tagCount);
   };
 
   /**
    *
    */
-   var getSearchResultRequest = function() {
+   const getSearchResultRequest = () => {
      _setHeadersToRequest(ECOpCodes.EC_OP_SEARCH_RESULTS);
      var tagCount = 0;
      _buildTagArrayBuffer(8, ECOpCodes.EC_TAGTYPE_UINT8, EC_SEARCH_TYPE.EC_SEARCH_LOCA, null);
      tagCount++;
-     return $._finalizeRequest(tagCount);
+     return _finalizeRequest(tagCount);
    };
 
   /**
    *
    */
-  var getDownloadsRequest = function() {
+  const getDownloadsRequest = () => {
     _setHeadersToRequest(13); // EC_OP_GET_DLOAD_QUEUE
-    return $._finalizeRequest(0);
+    return _finalizeRequest(0);
   };
 
   /**
    *
    */
-  var clearCompletedRequest = function() {
+  const clearCompletedRequest = () => {
     _setHeadersToRequest(0x53); // EC_OP_CLEAR_COMPLETED
-    return $._finalizeRequest(0);
+    return _finalizeRequest(0);
   };
 
   /**
    *
    */
-  var getStatsRequest = function() {
+  const getStatsRequest = () => {
     _setHeadersToRequest(10); // EC_OP_STAT_REQ
     var tagCount = 0;
     var EC_TAG_DETAIL_LEVEL = 4;
     var EC_DETAIL_INC_UPDATE = 4;
     _buildTagArrayBuffer(EC_TAG_DETAIL_LEVEL, ECOpCodes.EC_TAGTYPE_UINT8, EC_DETAIL_INC_UPDATE, null);
     tagCount++;
-    return $._finalizeRequest(tagCount);
+    return _finalizeRequest(tagCount);
   };
+
+  function ab2str(ab) {
+    var dataView = new DataView(ab);
+    var decoder = new TextDecoder('utf-8');
+    return decoder.decode(dataView);
+  }
+
+  function str2ab(str) {
+      var encoder = new TextEncoder('utf-8');
+      return encoder.encode(str).buffer;
+  }
 
   /**
    *
    */
-  var readSalt = function(buffer) {
+  const readSalt = (buffer) => {
     var offset = Uint32Array.BYTES_PER_ELEMENT * 2;
 
     var dataView = new DataView(buffer, offset, Uint8Array.BYTES_PER_ELEMENT);
@@ -362,38 +373,35 @@
   /**
    *
    */
-  var readBuffer = function(buffer, byteNumberToRead, littleEndian = false) {
-    var val = null;
+  const readBuffer = (buffer, byteNumberToRead, littleEndian = false) => {
+    let val = null;
+    // console.log('offset ' + offset);
     try {
-      var dataView = new DataView(buffer, offset, byteNumberToRead);
-      if(byteNumberToRead===1) {
+      const dataView = new DataView(buffer, offset, byteNumberToRead);
+      if(byteNumberToRead === 1) {
         val = dataView.getUint8(0);
-      } else if(byteNumberToRead===2) {
+      } else if(byteNumberToRead === 2) {
         val = dataView.getUint16(0, littleEndian);
-      } else if(byteNumberToRead===4) {
+      } else if(byteNumberToRead === 4) {
         val = dataView.getUint32(0, littleEndian);
       }
-      offset += byteNumberToRead;
-    }
-    catch(err) {
+    } catch (err) {
       console.log(err);
     }
+    offset += byteNumberToRead;
     return val;
   };
 
-  /**
-   *
-   */
-  var readBufferChildren = function(buffer, res, totalSizeOfRequest) {
+  const readBufferChildren = (buffer, res) => {
     res.children = [];
-    for (var j = 0; j < res.tagCountInResponse; j++) {
-      var child = {};
+    for (let j = 0; j < res.tagCountInResponse; j++) {
+      const child = {};
       child.nameEcTag = readBuffer(buffer, 2);
       child.typeEcOp = readBuffer(buffer, 1);
       // length without ectag, ecOp, length and tag count
       child.length = readBuffer(buffer, 4);
       // console.log('(child.length + offset): ' + (child.length + offset) + ' bytes, totalSizeOfRequest' + totalSizeOfRequest);
-      if(child.length + offset > totalSizeOfRequest) {
+      if(child.length + offset > res.totalSizeOfRequest) {
         console.log('ERROR: should not happen');
         return res;
       }
@@ -445,16 +453,13 @@
     return res;
   };
 
-  /**
-   *
-   */
-  var readResultsList = function(buffer) {
-    return new Promise(function(resolve, reject) {
-      var res = {};
+  const readResultsList = buffer => {
+    return new Promise((resolve, reject) => {
+      let res = {};
       offset = 0;
       res.header = readBuffer(buffer, 4);
       // length (total minus header and response Length)
-      var responseLength = readBuffer(buffer, 4);
+      let responseLength = readBuffer(buffer, 4);
       // response length (total minus header, response length, opcode, tag count)
       // res.responseLength = responseLength - 3;
       res.totalSizeOfRequest = responseLength + 6;// the 6 is deduce
@@ -471,150 +476,121 @@
       }
       console.log(res);
 
-      readBufferChildren(buffer, res, res.totalSizeOfRequest);
-      res.children.forEach(function(e) {
-        e.children.forEach(function(m) {
-          if(m.nameEcTag === 769) { // EC_TAG_PARTFILE_NAME
-            e.value = m.value;
-          }
-          if(m.nameEcTag === 798) { // EC_TAG_PARTFILE_HASH
-            e.hash = m.value;
-          }
-          if(m.nameEcTag === 771) { // EC_TAG_PARTFILE_SIZE_FULL
-            e.size = m.value;
-          }
-          if(m.nameEcTag === 782) { // EC_TAG_PARTFILE_ED2K_LINK
-            e.edkLink = m.value;
-          }
-        });
+      readBufferChildren(buffer, res);
+      res.children.forEach(e => {
+        if(e.children) {
+          e.children.forEach(m => {
+            if(m.nameEcTag === 769) { // EC_TAG_PARTFILE_NAME
+              e.value = m.value;
+            }
+            if(m.nameEcTag === 798) { // EC_TAG_PARTFILE_HASH
+              e.hash = m.value;
+            }
+            if(m.nameEcTag === 771) { // EC_TAG_PARTFILE_SIZE_FULL
+              e.size = m.value;
+            }
+            if(m.nameEcTag === 782) { // EC_TAG_PARTFILE_ED2K_LINK
+              e.edkLink = m.value;
+            }
+          });
+        }
       });
       console.log(res);
       resolve(res);
     });
   };
 
-  /**
-   * init TCP connection with amule
-   *
-   * @returns {Promise}
-   */
-  var initConnToServer = function(ip, port) {
-    return new Promise(function(resolve, reject) {
-      chrome.sockets.tcp.create({}, function(createInfo) {
+  const initConnToServer = (ip, port) => {
+    return new Promise((resolve, reject) => {
+      chrome.sockets.tcp.create({}, createInfo => {
         socketId = createInfo.socketId;
-        chrome.sockets.tcp.connect(createInfo.socketId, ip, port, function(resultCode) {
-          resolve();
-        });
+        chrome.sockets.tcp.connect(createInfo.socketId, ip, port, code => resolve(code));
       });
     });
   };
 
-  /**
-   * sent TCP request to amule
-   *
-   * @param data
-   * @returns {Promise}
-   */
-  var sendToServer_simple = function (data) {
-    return new Promise(function(resolve, reject) {
-      chrome.sockets.tcp.send(socketId, data, function(sendInfo) {});
-      chrome.sockets.tcp.onReceive.addListener(function(receiveInfo) {
-        resolve(receiveInfo.data);
-      });
+  const sendToServer_simple = data => {
+    return new Promise((resolve, reject) => {
+      chrome.sockets.tcp.send(socketId, data, sendInfo => {});
+      chrome.sockets.tcp.onReceive.addListener(receiveInfo => resolve(receiveInfo.data));
     });
   };
 
-  /**
-   *
-   */
-  var sendToServer = function (data) {
-    return new Promise(function(resolve, reject) {
-      var buf = [];
-      chrome.sockets.tcp.send(socketId, data, function(sendInfo) {});
-      chrome.sockets.tcp.onReceive.addListener(function(info) {
+  const sendToServer = data => {
+    return new Promise((resolve, reject) => {
+      let buf = [];
+      chrome.sockets.tcp.send(socketId, data, info => {});
+      chrome.sockets.tcp.onReceive.addListener(info => {
         buf.push(info.data);
       });
-      setTimeout(function() {
-        var bl = 0;
-        buf.forEach(function(b){
+      setTimeout(() => {
+        let bl = 0;
+        buf.forEach(b => {
           bl += b.byteLength;
         });
-        var buffer = new ArrayBuffer(bl);
-        var o = 0;
-        buf.forEach(function(b){
+        const buffer = new ArrayBuffer(bl);
+        let o = 0;
+        buf.forEach(b => {
           for (var j = 0; j < b.byteLength; j++) {
-            var fromArrayView = new Uint8Array(b, j, 1);
-            var toArrayView = new Uint8Array(buffer, j + o, 1);
+            let fromArrayView = new Uint8Array(b, j, 1);
+            let toArrayView = new Uint8Array(buffer, j + o, 1);
             toArrayView.set(fromArrayView);
           }
-          o = o +b.byteLength;
+          o = o + b.byteLength;
         });
+        console.log('response size in bit: '+ o + ' number of packet: ' + buf.length);
         resolve(buffer);
       }, 3000);
     });
   };
 
-  /**
-   *
-   */
-  var connect = function(ip, port, password, md5) {
-    return new Promise(function(resolve, reject) {
-      initConnToServer(ip, port).then(function() {
-        init(password, md5);
-        return sendToServer_simple(getAuthRequest1());
-      })
-      .then(function(data) {
-        readSalt(data);
-        return sendToServer_simple($.getAuthRequest2());
-      })
-      .then(function(data) {
-        if (readSalt(data) === 4) {
-          resolve('You are successfuly connected to amule');
-        }
-        else {
-          reject('You are NOT connected to amule');
-        }
-      })
-      .catch(function (err) {
-        console.log("error: ", err);
-        reject('You are NOT connected to amule: ' + err);
-      });
+  const connect = (ip, port, password, md5) => {
+    return initConnToServer(ip, parseInt(port)).then(() => {
+      _init(password, md5);
+      return sendToServer_simple(getAuthRequest1());
+    }).then(data => {
+      readSalt(data);
+      return sendToServer_simple(_getAuthRequest2());
+    }).then(data => {
+      if (readSalt(data) === 4) {
+        return('You are successfuly connected to amule');
+      }
+      else {
+        throw('You are NOT connected to amule');
+      }
+    })
+    .catch(err => {
+      throw('You are NOT connected to amule: ' + err);
     });
   };
 
   /**
    * send a search request
    */
-  var search = function(q) {
-    return sendToServer($.getSearchStartRequest(q));
-  };
+  const search = q => sendToServer(_getSearchStartRequest(q));
 
-  var fetchSearch = function() {
-    return sendToServer(getSearchResultRequest()).then(function (data) {
-     return readResultsList(data);
-    });
-  };
+  const fetchSearch = () => sendToServer(getSearchResultRequest()).then(data => readResultsList(data));
 
-  var getDownloads = function() {
-    return sendToServer(getDownloadsRequest()).then(function (data) {
+  const getDownloads = () => {
+    return sendToServer(getDownloadsRequest()).then(data => {
       return readResultsList(data);
     });
   };
 
-  var getSharedFiles = function() {
-    return sendToServer(getSharedFilesRequest()).then(function (data) {
+  const getSharedFiles = () => {
+    return sendToServer(getSharedFilesRequest()).then(data => {
       return readResultsList(data);
     });
   };
 
-  var clearCompleted = function() {
-    return sendToServer_simple(clearCompletedRequest()).then(function (data) {
+  const clearCompleted = () => {
+    return sendToServer(clearCompletedRequest()).then(data => {
       return readResultsList(data);
     });
   };
 
-  var getStats = function() {
-    return sendToServer_simple(getStatsRequest()).then(function (data) {
+  const getStats = () => {
+    return sendToServer(getStatsRequest()).then(data => {
       return readResultsList(data);
     });
   };
